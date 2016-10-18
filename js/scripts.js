805836c0588c45b8387b10409d7b76a8bda2bc12
Brad Copenhaver and Brian Pritt <brian@>
2016-10-18 16:32:15 -0700

    add more functionality such as removing text.  Also adds JS functions as a means of reducing code lenght(unsuccessfully

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,56 @@
+var catIcon = function() {//adds a small cat to screen when clicked
+  $("ul#smallCats").after("<img src='img/catsmall.png' class='icon'>");
+  $(".icon").click(function(){
+    $(this).remove();
+});
+};
+var dogIcon = function(){//adds a small dog to screen when clicked
+  $("ul#smallDogs").after("<img src='img/dogsmall.png' class='icon'>");
+  $(".icon").click(function(){
+    $(this).remove();
+    });
+};
+
 $(document).ready(function() {
   $("button#meow").click(function() {
-    $("img").remove();
+    $("img").remove();//clears img 'buffer'
     $("ul#callout").prepend("<li>Meeoww!</li>");
     $("ul#reply").prepend("<li>Woof woof!!</li>");
     $("ul#callout").before("<img src='img/cat.png' id='kitty'>");
     $("ul#reply").before("<img src='img/dog.jpg' id='puppy'>");
 
     $("img#kitty").click(function(){
-      $("img#kitty").after("<img src='img/catsmall.png' class='icon'>");
-      $(".icon").click(function(){
-        $(this).remove();
-        });
+      catIcon();
     });
     $("img#puppy").click(function(){
-      $("img#puppy").after("<img src='img/dogsmall.png' class='icon'>");
-      $(".icon").click(function(){
-        $(this).remove();
-        });
+      dogIcon();
     });
-
-
+    $("ul#callout").children("li").first().click(function(){
+      $(this).remove();
+    });
+    $("ul#reply").children("li").first().click(function(){
+      $(this).remove();
+    });
   });
 
   $("button#woof").click(function() {
-    $("img").remove();
+    $("img").remove();//clear img 'buffer'
     $("ul#callout").prepend("<li>Woof!</li>");
     $("ul#reply").prepend("<li>Meow-ow!</li>");
     $("ul#callout").before("<img src='img/dog.jpg' id='puppy'>");
     $("ul#reply").before("<img src='img/cat.png'id='kitty'>");
 
     $("img#kitty").click(function(){
-      $("img#kitty").after("<img src='img/catsmall.png' class='icon'>");
-      $(".icon").click(function(){
-        $(this).remove();
-        });
+      catIcon();
     });
     $("img#puppy").click(function(){
-      $("img#puppy").after("<img src='img/dogsmall.png' class='icon'>");
-      $(".icon").click(function(){
-        $(this).remove();
-        });
+      dogIcon();
     });
+    $("ul#callout").children("li").first().click(function(){
+      $(this).remove();
     });
-
-
-
+    $("ul#reply").children("li").first().click(function(){
+      $(this).remove();
+    });
+  });
 });
